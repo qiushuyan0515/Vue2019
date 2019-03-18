f5abfad38c995eeb910cea6dc5b3ca81ce20d446
qiushuyan0515 <qiushuyan0515@163.com>
2019-03-18 15:10:43 +0800

    eleme-master

---
+++ eleme-master/src/components/goods/goods1.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="good">
+        <div class="menu-wrapper" ref="menuWrapper">
+            <ul>
+                <li class="menu-item border-1px">
+                       <span class="text">
+                           <span class="icon"></span>
+                       </span> 
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
